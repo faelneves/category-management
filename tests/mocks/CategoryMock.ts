2e42b80c3cde59fb56914a9cab109ce22fd471fb
Rafael Neves <rafaelneves.130@gmail.com>
2024-11-14 00:28:58 -0300

    refactor: fix category mock variable name

--- tests/mocks/CategoryMock.ts
+++ tests/mocks/CategoryMock.ts
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker';
 import { ICategory } from './../../src/domain/entities/ICategory';
 
-export const CategoryMock: ICategory = {
+export const categoryMock: ICategory = {
   id: faker.string.uuid(),
   name: faker.lorem.word(),
   hierarchyLevel: faker.number.int({ min: 1, max: 5 }),
--- tests/unit/infrastructure/prisma/repositories/PrismaCategoryRepository.test.ts
+++ tests/unit/infrastructure/prisma/repositories/PrismaCategoryRepository.test.ts
@@ -1,6 +1,6 @@
 import { PrismaCategoryRepository } from './../../../../../src/infrastructure/prisma/repositories/PrismaCategoryRepository';
 import prismaInstance from '../../../../../src/infrastructure/prisma/PrismaClient';
-import { CategoryMock } from '../../../../mocks/CategoryMock';
+import { categoryMock } from '../../../../mocks/CategoryMock';
 import HttpError from '../../../../../src/domain/exceptions/HttpError';
 
 jest.mock('@prisma/client', () => {
@@ -25,7 +25,7 @@ describe('PrismaCategoryRepository', () => {
       prismaInstance.category.create = jest.fn().mockImplementationOnce((category) => category);
       const instance = new PrismaCategoryRepository(prismaInstance);
 
-      const createdCategory = await instance.create(CategoryMock);
+      const createdCategory = await instance.create(categoryMock);
 
       expect(createdCategory).toEqual(createdCategory);
     });
@@ -34,43 +34,43 @@ describe('PrismaCategoryRepository', () => {
       prismaInstance.category.create = jest.fn().mockRejectedValueOnce(new Error('Prisma error'));
       const instance = new PrismaCategoryRepository(prismaInstance);
 
-      await expect(instance.create(CategoryMock)).rejects.toThrow(new Error('Prisma error'));
+      await expect(instance.create(categoryMock)).rejects.toThrow(new Error('Prisma error'));
     });
   });
 
   describe('findById', () => {
     it('should return a category with success', async () => {
-      prismaInstance.category.findUnique = jest.fn().mockResolvedValueOnce(CategoryMock);
+      prismaInstance.category.findUnique = jest.fn().mockResolvedValueOnce(categoryMock);
       const instance = new PrismaCategoryRepository(prismaInstance);
 
-      const foundCategory = await instance.findById(CategoryMock.id);
+      const foundCategory = await instance.findById(categoryMock.id);
 
-      expect(foundCategory).toEqual(CategoryMock);
+      expect(foundCategory).toEqual(categoryMock);
     });
 
     it('should throw an http error when prisma returns null', async () => {
       prismaInstance.category.findUnique = jest.fn().mockResolvedValueOnce(null);
       const instance = new PrismaCategoryRepository(prismaInstance);
 
-      await expect(instance.findById(CategoryMock.id)).rejects.toThrow(new HttpError('Category not found.', 404));
+      await expect(instance.findById(categoryMock.id)).rejects.toThrow(new HttpError('Category not found.', 404));
     });
   });
 
   describe('findByParentId', () => {
     it('should return an array of categories', async () => {
-      prismaInstance.category.findMany = jest.fn().mockResolvedValueOnce([CategoryMock]);
+      prismaInstance.category.findMany = jest.fn().mockResolvedValueOnce([categoryMock]);
       const instance = new PrismaCategoryRepository(prismaInstance);
 
-      const foundCategories = await instance.findByParentId(CategoryMock.id);
+      const foundCategories = await instance.findByParentId(categoryMock.id);
 
-      expect(foundCategories).toEqual([CategoryMock]);
+      expect(foundCategories).toEqual([categoryMock]);
     });
 
     it('should throw an error when prisma throws', async () => {
       prismaInstance.category.findMany = jest.fn().mockRejectedValueOnce(new Error('Prisma error'));
       const instance = new PrismaCategoryRepository(prismaInstance);
 
-      await expect(instance.findByParentId(CategoryMock.id)).rejects.toThrow(new Error('Prisma error'));
+      await expect(instance.findByParentId(categoryMock.id)).rejects.toThrow(new Error('Prisma error'));
     });
   });
 
@@ -79,16 +79,16 @@ describe('PrismaCategoryRepository', () => {
       prismaInstance.category.delete = jest.fn();
       const instance = new PrismaCategoryRepository(prismaInstance);
 
-      await instance.delete(CategoryMock.id);
+      await instance.delete(categoryMock.id);
 
-      expect(prismaInstance.category.delete).toHaveBeenCalledWith({ where: { id: CategoryMock.id } });
+      expect(prismaInstance.category.delete).toHaveBeenCalledWith({ where: { id: categoryMock.id } });
     });
 
     it('should throw an error when prisma throws', async () => {
       prismaInstance.category.delete = jest.fn().mockRejectedValueOnce(new Error('Prisma error'));
       const instance = new PrismaCategoryRepository(prismaInstance);
 
-      await expect(instance.delete(CategoryMock.id)).rejects.toThrow(new Error('Prisma error'));
+      await expect(instance.delete(categoryMock.id)).rejects.toThrow(new Error('Prisma error'));
     });
   });
 });
