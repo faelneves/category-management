be8e3fb04f7e12da9ba14a4ff522ee63f22242de
Rafael Neves <rafaelneves.130@gmail.com>
2024-11-13 21:17:25 -0300

    feat: implements route to create category

--- src/application/v1/routerV1.ts
+++ src/application/v1/routerV1.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import PingControllerFactory from '../../infrastructure/factories/controller/PingControllerFactory';
+import CreateCategoryControllerFactory from '../../infrastructure/factories/controller/CreateCategoryControllerFactory';
 
 const routerV1 = Router();
 
 (async () => {
   routerV1.get('/ping', PingControllerFactory.make().execute);
+  routerV1.post('/category', CreateCategoryControllerFactory.make().execute);
 })();
 
 export default routerV1;
